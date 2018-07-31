9256bd7ae53f8947888574f50d507803fa27a993
Phil Banks <phil@helpfultechnology.com>
2018-07-31 21:33:01 +0100

    update
    - handle ping fail better - 0 instead of ""
    - add wifi / lan test
    - remove " " in output
    - fix typo in log file name

--- home/pi/updown.sh
+++ home/pi/updown.sh
@@ -1,21 +1,32 @@
 #!/bin/bash
 
 # Define log files.
-log_domain='/var/log/updown_dommain.log';
+log_domain='/var/log/updown_domain.log';
 log_dns='/var/log/updown_dns.log';
+log_wifi='/var/log/updown_wifi.log'
 
 # Create log files as needed.
 test -f $log_domain || echo $'\n' > $log_domain;
 test -f $log_dns || echo $'\n' > $log_dns;
+test -f $log_wifi || echo $'\n' > $log_wifi;
 
 # Define targets;
 domain_target='google.co.uk';
 dns_target='1.1.1.1';
+wifi_target='192.168.1.1';
 
 # Run the tests.
-domain_test=$( ping -f -c 10 $domain_target | awk -F '/' 'END {print strftime("%s,"), $5}' );
-dns_test=$( ping -f -c 10 $dns_target | awk -F '/' 'END {print strftime("%s,"), $5}' );
+domain_test=$( sudo ping -f -c 10 192.168.1.2 | awk -F '/' 'END {print $5}' );
+  [[ -z "$domain_test" ]] && domain_test=0;
+  domain_test=$(date +%s)","$domain_test;
+dns_test=$( sudo ping -f -c 10 192.168.1.2 | awk -F '/' 'END {print $5}' );
+  [[ -z "$dns_test" ]] && dns_test=0;
+  dns_test=$(date +%s)","$dns_test;
+wifi_test=$( sudo ping -f -c 10 192.168.1.2 | awk -F '/' 'END {print $5}' );
+  [[ -z "$wifi_test" ]] && wifi_test=0;
+  wifi_test=$(date +%s)","$wifi_test;
 
 # Write the results. Currently cron is every 5 mins. 288 = 24 hours worth.
 sed -ni "1s/^/$domain_test\n/;288q;p" $log_domain;
 sed -ni "1s/^/$dns_test\n/;288q;p" $log_dns;
+sed -ni "1s/^/$wifi_test\n/;288q;p" $log_wifi;
